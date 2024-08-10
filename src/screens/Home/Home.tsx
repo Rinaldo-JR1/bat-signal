6e40c60241ce4f511114aeda7839cebcfe00a667
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-08-10 11:36:29 -0300

    Home page setIsForm

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,12 +4,16 @@ import { Text, View } from 'react-native';
 import { styles } from './Styles';
 import { BatLogo } from '../../components/Bat-logo/Bat-logo';
 import { BatButton } from '../../components/Bat-button/Bat-Button';
+import { ScreenProps } from '../../interfaces/ScreenProps/ScreenProps';
 
-export function Home() {
+export function Home({ setIsForm }: ScreenProps) {
+  const emitSignal = () => {
+    setIsForm(true)
+  }
   return (
     <View style={styles.container}>
       <BatLogo />
-      <BatButton />
+      <BatButton onPress={emitSignal} />
     </View>
   );
 }

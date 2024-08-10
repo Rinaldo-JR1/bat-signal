280998951b21d091ccc920a5c5bc979135296a62
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-08-10 11:37:33 -0300

    Props

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens/Home/Home';
+import { useState } from 'react';
+import { BatForm } from './src/screens/Bat-Form/Bat-Form';
 
 export default function App() {
+  const [isForm, setIsForm] = useState<boolean>(false);
+  const handleForm = () => {
+    if (isForm) {
+      return <BatForm setIsForm={setIsForm} />
+    } else {
+      return <Home setIsForm={setIsForm} />
+    }
+  }
   return (
     <>
       <StatusBar style="auto" />
-      <Home />
+      {handleForm()}
     </>
   );
 }
---
+++ src/interfaces/ScreenProps/ScreenProps.ts
@@ -0,0 +1,3 @@
+export interface ScreenProps {
+  setIsForm: (isForm: boolean) => void;
+}
